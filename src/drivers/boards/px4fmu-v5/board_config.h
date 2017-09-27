junwoo091400/PX4-Autopilot

px4fmu-v5:Add FMUv5 HW revision and version

src/drivers/boards/px4fmu-v5/board_config.h
@@ -311,9 +311,15 @@
 
 /* HW Version and Revision drive signals Default to 1 to detect */
 
-#define GPIO_HW_REV_DRIVE    /* PH14  */ (GPIO_OUTPUT|GPIO_PUSHPULL|GPIO_SPEED_2MHz|GPIO_OUTPUT_SET|GPIO_PORTH|GPIO_PIN14)
-#define GPIO_HW_VER_DRIVE    /* PG0   */ (GPIO_OUTPUT|GPIO_PUSHPULL|GPIO_SPEED_2MHz|GPIO_OUTPUT_SET|GPIO_PORTG|GPIO_PIN0)
+#define BOARD_HAS_HW_VERSIONING
 
+#define GPIO_HW_REV_DRIVE    /* PH14  */ (GPIO_OUTPUT|GPIO_PUSHPULL|GPIO_SPEED_2MHz|GPIO_OUTPUT_SET|GPIO_PORTH|GPIO_PIN14)
+#define GPIO_HW_REV_SENCE    /* PC3   */ ADC1_GPIO(13)
+#define GPIO_HW_VER_DRIVE    /* PG0   */ (GPIO_OUTPUT|GPIO_PUSHPULL|GPIO_SPEED_2MHz|GPIO_OUTPUT_SET|GPIO_PORTG|GPIO_PIN0)
+#define GPIO_HW_VER_SENCE    /* PC2   */  ADC1_GPIO(12)
+#define HW_INFO_INIT {'V','5','x', 'x',0}
+#define HW_INFO_INIT_REV       2
+#define HW_INFO_INIT_VER            3
 /* CAN Silence
  *
  * Silent mode control \ ESC Mux select

picli3/Firmware

src/drivers/boards/px4fmu-v5/board_config.h
@@ -311,9 +311,15 @@
 
 /* HW Version and Revision drive signals Default to 1 to detect */
 
-#define GPIO_HW_REV_DRIVE    /* PH14  */ (GPIO_OUTPUT|GPIO_PUSHPULL|GPIO_SPEED_2MHz|GPIO_OUTPUT_SET|GPIO_PORTH|GPIO_PIN14)
-#define GPIO_HW_VER_DRIVE    /* PG0   */ (GPIO_OUTPUT|GPIO_PUSHPULL|GPIO_SPEED_2MHz|GPIO_OUTPUT_SET|GPIO_PORTG|GPIO_PIN0)
+#define BOARD_HAS_HW_VERSIONING
 
+#define GPIO_HW_REV_DRIVE    /* PH14  */ (GPIO_OUTPUT|GPIO_PUSHPULL|GPIO_SPEED_2MHz|GPIO_OUTPUT_SET|GPIO_PORTH|GPIO_PIN14)
+#define GPIO_HW_REV_SENSE    /* PC3   */ ADC1_GPIO(13)
+#define GPIO_HW_VER_DRIVE    /* PG0   */ (GPIO_OUTPUT|GPIO_PUSHPULL|GPIO_SPEED_2MHz|GPIO_OUTPUT_SET|GPIO_PORTG|GPIO_PIN0)
+#define GPIO_HW_VER_SENSE    /* PC2   */ ADC1_GPIO(12)
+#define HW_INFO_INIT {'V','5','x', 'x',0}
+#define HW_INFO_INIT_REV       2
+#define HW_INFO_INIT_VER            3
 /* CAN Silence
  *
  * Silent mode control \ ESC Mux select