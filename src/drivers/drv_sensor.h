PX4/PX4-Autopilot

drv_sensor:Fix merge by adding new NXP sensors after what was on master

src/drivers/drv_sensor.h
@@ -90,16 +90,15 @@
 #define DRV_GYR_DEVTYPE_BMI055		0x42
 #define DRV_MAG_DEVTYPE_BMM150		0x43
 #define DRV_BARO_DEVTYPE_BMP285		0x44
-#define DRV_BARO_DEVTYPE_MPL3115A2	0x45
-#define DRV_ACC_DEVTYPE_FXOS8700C	0x46
-#define DRV_MAG_DEVTYPE_FXOS8700C	0x47
-
 #define DRV_DIFF_PRESS_DEVTYPE_ETS3	0x45
 #define DRV_DIFF_PRESS_DEVTYPE_MS4525	0x46
 #define DRV_DIFF_PRESS_DEVTYPE_MS5525	0x47
 #define DRV_DIFF_PRESS_DEVTYPE_SDP31	0x48
 #define DRV_DIFF_PRESS_DEVTYPE_SDP32	0x49
 #define DRV_DIFF_PRESS_DEVTYPE_SDP33	0x50
+#define DRV_BARO_DEVTYPE_MPL3115A2	0x51
+#define DRV_ACC_DEVTYPE_FXOS8700C	0x52
+#define DRV_MAG_DEVTYPE_FXAS2100C	0x53
 
 /*
  * ioctl() definitions

picli3/Firmware

src/drivers/drv_sensor.h
@@ -90,16 +90,16 @@
 #define DRV_GYR_DEVTYPE_BMI055		0x42
 #define DRV_MAG_DEVTYPE_BMM150		0x43
 #define DRV_BARO_DEVTYPE_BMP285		0x44
-#define DRV_BARO_DEVTYPE_MPL3115A2	0x45
-#define DRV_ACC_DEVTYPE_FXOS8700C	0x46
-#define DRV_MAG_DEVTYPE_FXOS8700C	0x47
-
 #define DRV_DIFF_PRESS_DEVTYPE_ETS3	0x45
 #define DRV_DIFF_PRESS_DEVTYPE_MS4525	0x46
 #define DRV_DIFF_PRESS_DEVTYPE_MS5525	0x47
 #define DRV_DIFF_PRESS_DEVTYPE_SDP31	0x48
 #define DRV_DIFF_PRESS_DEVTYPE_SDP32	0x49
 #define DRV_DIFF_PRESS_DEVTYPE_SDP33	0x50
+#define DRV_BARO_DEVTYPE_MPL3115A2	0x51
+#define DRV_ACC_DEVTYPE_FXOS8700C	0x52
+#define DRV_MAG_DEVTYPE_FXOS8700C	0x53
+#define DRV_GYR_DEVTYPE_FXAS2100C	0x54
 
 /*
  * ioctl() definitions